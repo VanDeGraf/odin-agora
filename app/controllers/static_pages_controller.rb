3b393ea9da2fa21a466bc2a6d3ce802cc2ace89d
VanDeGraf <vandegraf@bk.ru>
2021-11-03 08:17:23 +0400

    Add Static Pages Controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def people; end
+
+  def feed; end
+
+  def about; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root to: "static_pages#feed"
+
   devise_for :users
   get "/users/:id", to: "users#profile"
   get "/users/:id/friends", to: "users#friends"
+
+  get "/people", to: "static_pages#people"
+  get "/feed", to: "static_pages#feed"
+  get "/about", to: "static_pages#about"
 end
