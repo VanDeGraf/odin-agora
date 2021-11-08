b2d121885374158bc329cd2a5bee68d964ecae24
VanDeGraf <vandegraf@bk.ru>
2021-11-08 11:50:19 +0400

    Add additional User params to default devise registration views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # ---------------------------------------
   # Devise autocomplete helpers
@@ -7,5 +8,19 @@ class ApplicationController < ActionController::Base
   # @return [User]
   def current_user
     super
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    keys = %i[
+      first_name
+      last_name
+      sex
+      birthday
+      avatar_url
+    ].freeze
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
   end
 end
