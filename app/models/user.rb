0a9471c235972827d69829f159f8a7f866785dc2
VanDeGraf <vandegraf@bk.ru>
2021-11-08 15:12:47 +0400

    Add on create omniauth user confirmed_at value to ignore confirmation stage

--- app/models/user.rb
+++ app/models/user.rb
@@ -141,6 +141,7 @@ class User < ApplicationRecord
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
+      user.confirmed_at = DateTime.now.prev_day
       # user.password = Devise.friendly_token[0, 20]
     end
   end
