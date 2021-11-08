146cf536cbe1f0e474370b697efdb8fc20f0f48b
VanDeGraf <vandegraf@bk.ru>
2021-11-08 05:10:00 +0400

    Add people show logic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,20 @@
 class StaticPagesController < ApplicationController
-  def people; end
+  def people
+    join_statement = <<~SQL
+         LEFT JOIN friends f ON
+      f.friend_id = users.id OR f.other_friend_id = users.id
+    SQL
+    where_statement = <<~SQL
+      users.id NOT IN (
+          SELECT u.id
+          FROM users as u
+                   INNER JOIN friends f2 ON
+                  (f2.friend_id = u.id OR f2.other_friend_id = u.id)
+                  AND (f2.friend_id = #{current_user.id} OR f2.other_friend_id = #{current_user.id})
+      )
+    SQL
+    @people = User.joins(join_statement).where(where_statement).group('id').order('COUNT(id)')
+  end
 
   def feed
     posts = Post.distinct.includes(:author).joins(friends_children_join('posts')).order('created_at DESC')
