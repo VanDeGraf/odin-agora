3c05586e10f3e4c520e2c00bdc7b5d9b061f4e5f
VanDeGraf <vandegraf@bk.ru>
2021-11-08 05:26:05 +0400

    Add Post creation logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,16 @@ class PostsController < ApplicationController
     @post = Post.includes(:author, :likes, comments: [:author]).find(params[:id])
   end
 
-  def new; end
+  def new
+    @post = Post.new
+  end
 
-  def create; end
+  def create
+    permitted = params.required(:post).permit(:title, :body)
+    @post = current_user.posts.create(title: permitted[:title], body: permitted[:body])
+    flash[:notice] = 'Successfully post create'
+    redirect_to @post
+  end
 
   def create_comment; end
 end
