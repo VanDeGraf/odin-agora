2b25014b1f2aa7771771e78bc91aae4ae90db8f7
VanDeGraf <vandegraf@bk.ru>
2021-11-18 17:51:57 +0400

    Add test factories with user association (as author)

--- test/factories/comments.rb
+++ test/factories/comments.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :comment do
     body { Faker::Lorem.paragraph(sentence_count: 2, random_sentences_to_add: 10) }
+
+    factory :comment_with_author do
+      association :author, factory: :user
+    end
   end
 end
--- test/factories/posts.rb
+++ test/factories/posts.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :post do
     title { Faker::Lorem.sentence(word_count: 2, random_words_to_add: 2) }
     body { Faker::Lorem.paragraph(sentence_count: 4, random_sentences_to_add: 30) }
+
+    factory :post_with_author do
+      association :author, factory: :user
+    end
   end
 end
