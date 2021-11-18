de79e1c99d5497529834cdeb96d0a458e20b3ba7
VanDeGraf <vandegraf@bk.ru>
2021-11-18 18:05:40 +0400

    Add some Message model tests and factories

--- test/factories/messages.rb
+++ test/factories/messages.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :message do
     body { Faker::Lorem.paragraph(sentence_count: 2, random_sentences_to_add: 10) }
+
+    factory :message_with_sender do
+      association :sender, factory: :user
+    end
+    factory :message_with_recipient do
+      association :recipient, factory: :user
+
+      factory :message_with_sender_and_recipient do
+        association :sender, factory: :user
+      end
+    end
   end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid' do
+    message = FactoryBot.build(:message_with_sender_and_recipient)
+    assert message.valid?, message.errors.full_messages
+  end
+  test 'can save' do
+    message = FactoryBot.build(:message_with_sender_and_recipient)
+    message.save
+    assert_not_nil(message.id, message.errors.full_messages)
+  end
+  test 'sender association' do
+    user = FactoryBot.create(:user)
+    message = FactoryBot.create(:message_with_recipient, sender_id: user.id)
+    assert_kind_of(User, message.sender)
+    assert_equal(message.sender.id, user.id)
+  end
+  test 'recipient association' do
+    user = FactoryBot.create(:user)
+    message = FactoryBot.create(:message_with_sender, recipient_id: user.id)
+    assert_kind_of(User, message.recipient)
+    assert_equal(message.recipient.id, user.id)
+  end
 end
